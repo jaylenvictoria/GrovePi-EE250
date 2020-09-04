05ffeb99e7de597cc73f5c2dd56ffaa945bea75a
Jaylen Jackson <jvjackso@usc.edu>
2020-09-04 02:24:02 -0700

    python code

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -36,3 +36,89 @@ if __name__ == '__main__':
         time.sleep(0.2)
 
         print(grovepi.ultrasonicRead(PORT))
+
+#connect grove rotary angle senssor to analog port A1
+potentiometer = 1
+
+grovepi.pinMode(potentiometer,"INPUT")
+time.sleep(1)
+
+#adc refrence voltage
+adc_ref = 5
+
+#vcc of grove interface
+grove_vcc = 5
+
+#the full angle of the rotary angle is 300 degrees
+full_angle = 300
+
+#range of ultrasonic 
+ultrasonic_range = 518
+
+#connect ultrasonic ranger to D7
+ultrasonic_ranger = 7
+
+
+if __name__ == '__main__':
+#background color
+	setRGB(0, 128, 0)
+	while True:
+		try:
+			#read sensor value from potentiometer
+			sensor_value = grovepi.analogRead(potentiometer)
+			
+			#calculate voltage
+			voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+			
+			#calculate rotation in degrees
+			degrees = round((voltage * full_angle) / grove_vcc, 2)
+
+			#convert to range from degrees
+			target = int(degrees / full_angle * ultrasonic_range)
+
+			#distance from ranger
+			distance = grovepi.ultrasonicRead(ultrasonic_ranger))
+
+			if distance < target:
+				setRGB( 128, 0, 0)
+				display = '%3dcm OBJ PRES   /n%3dcm' % (target, distance)
+			else
+				setRGB( 0, 128, 0)
+				display = '%3dcm OBJ PRES   /n%3dcm' % (target, distance)
+
+			setText_norefresh(display)
+
+		except KeyboardInerrupt:
+			#turn it off
+			setRGB(0,0,0)
+			setText('')
+			break
+
+		except I0Error:
+			print("Error")
+			setText('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
